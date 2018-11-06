b7bc05a4378c74e6097585a03e410475e2a61f3b
原鑫 <1017123610@qq.com>
2018-11-06 16:39:54 +0800

    #

--- src/components/blogIndex.vue
+++ src/components/blogIndex.vue
@@ -32,34 +32,7 @@
         <sir-submenu class="submenu">关于</sir-submenu>
       </div>
     </sir-navmenu>
-    <div class="headertop">
-      <div class="focusinfo">
-        <h1 class="center-text glitch is-glitching Ubuntu-font" data-text="Hi, Mashiro!">Hi, Mashiro!</h1>
-        <div class="header-info">
-          <p>
-            <i class="fa fa-quote-left"></i> You got to put the past behind you before you can move on.
-            <i class="fa fa-quote-right"></i>
-          </p>
-          <div class="top-social_v2">
-            <ul class="top-social-logo">
-              <li><img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.9/img/Sakura/images/sns/wechat.png" alt=""></li>
-              <li><img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.9/img/Sakura/images/sns/wechat.png" alt=""></li>
-              <li><img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.9/img/Sakura/images/sns/wechat.png" alt=""></li>
-              <li><img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.9/img/Sakura/images/sns/wechat.png" alt=""></li>
-              <li><img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.9/img/Sakura/images/sns/wechat.png" alt=""></li>
-              <li><img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.9/img/Sakura/images/sns/wechat.png" alt=""></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <sir-scrolltop></sir-scrolltop>
-      <div class="bolang1"></div>
-      <!-- 波浪的两个图片-->
-      <div class="bolang2"></div>
-    </div>
-    <router-view/>
-    <router-view/>
-    <router-view/>
+
     <router-view/>
   </div>
 </template>
@@ -136,77 +109,5 @@ export default {
   font-weight: 600;
   font-size: 14px;
   margin-left: 28px;
-}
-
-.headertop {
-  width: 100%;
-  height: 100%;
-  background: url('https://cdn.jsdelivr.net/gh/moezx/cdn@3.5.2/cover/webp/199-bigskin-3.jpg.webp') no-repeat;
-  background-size: cover;
-  position: relative;
-  z-index: -1;
-}
-
-.bolang1,
-.bolang2 {
-  height: 75px;
-  width: 100%;
-  background: url('../assets/image/wave1.png');
-  position: absolute;
-  bottom: 0;
-}
-
-.bolang2 {
-  bottom: 20px;
-  left: -50px;
-  background: url('../assets/image/wave2.png') repeat-x;
-}
-
-.focusinfo {
-  width: 800px;
-  height: 230px;
-  position: absolute;
-  top: calc(50% - 115px);
-  left: calc(50% - 400px);
-}
-
-.center-text {
-  width: 615px;
-  height: 100px;
-  font-size: 80px;
-  margin: 0 auto;
-  text-align: center;
-  color: white;
-  font-family: 'Ubuntu', sans-serif;
-}
-
-.header-info {
-  padding: 10px;
-  width: 500px;
-  height: auto;
-  margin: 20px auto;
-  text-align: center;
-  background: black;
-  opacity: 0.6;
-  border-radius: 10px;
-  font-size: 16px;
-  color: #eaeadf;
-  font-weight: 600;
-}
-.header-info p{
-  margin: 0 0 15px 0;
-}
-.top-social_v2 {
-  height: 100%;
-}
-
-.top-social-logo {
-  height: 40px;
-}
-
-.top-social-logo li {
-  width: 60px;
-  height: 40px;
-  float: left;
 }
 </style>
--- src/components/myIndex/index.vue
+++ src/components/myIndex/index.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="index">
-    <sir-table :data="tableData5">
+     <div class="headertop">
+      <div class="focusinfo">
+        <h1 class="center-text glitch is-glitching Ubuntu-font" data-text="Hi, Mashiro!">Hi, Mashiro!</h1>
+        <div class="header-info">
+          <p>
+            <i class="fa fa-quote-left"></i> You got to put the past behind you before you can move on.
+            <i class="fa fa-quote-right"></i>
+          </p>
+          <div class="top-social_v2">
+            <ul class="top-social-logo">
+              <li><img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.9/img/Sakura/images/sns/wechat.png" alt=""></li>
+              <li><img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.9/img/Sakura/images/sns/wechat.png" alt=""></li>
+              <li><img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.9/img/Sakura/images/sns/wechat.png" alt=""></li>
+              <li><img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.9/img/Sakura/images/sns/wechat.png" alt=""></li>
+              <li><img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.9/img/Sakura/images/sns/wechat.png" alt=""></li>
+              <li><img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.9/img/Sakura/images/sns/wechat.png" alt=""></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <sir-scrolltop></sir-scrolltop>
+      <div class="bolang1"></div>
+      <!-- 波浪的两个图片-->
+      <div class="bolang2"></div>
+    </div>
+     <sir-table :data="tableData5">
       <el-table-column label="商品 ID" prop="id">
       </el-table-column>
       <el-table-column label="商品名称" prop="name">
@@ -15,6 +40,7 @@ export default {
   // name: 'HelloWorld',
   data () {
     return {
+      activeIndex: '1',
       tableData5: [{
         id: '12987122',
         name: '好滋好味鸡蛋仔',
@@ -35,13 +61,104 @@ export default {
     }
   },
   methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    getlistdata () {
+      this.$ajax.get('/url').then(function (res) {
+        console.log(res)
+      }).catch(function (err) {
+        console.log(err)
+      })
+    }
+  },
+  mounted () {
+    this.$ajax.get('/getdata', {
+      params: { // 请求参数
+        id: 123
+      }
+    }).then(function (response) {
+      console.log(response)// 请求正确时执行的代码
+    }).catch(function (response) {
+      console.log(response)// 发生错误时执行的代码
+    })
   }
 }
 </script>
 <style scoped>
-.index {
+.index{
+    width: 100%;
+  height: 100%;
+}
+.headertop {
+  width: 100%;
+  height: 100%;
+  background: url('https://cdn.jsdelivr.net/gh/moezx/cdn@3.5.2/cover/webp/199-bigskin-3.jpg.webp') no-repeat;
+  background-size: cover;
+  position: relative;
+  z-index: -1;
+}
+
+.bolang1,
+.bolang2 {
+  height: 75px;
+  width: 100%;
+  background: url('../../assets/image/wave1.png');
+  position: absolute;
+  bottom: 0;
+}
+
+.bolang2 {
+  bottom: 20px;
+  left: -50px;
+  background: url('../../assets/image/wave2.png') repeat-x;
+}
+
+.focusinfo {
   width: 800px;
+  height: 230px;
+  position: absolute;
+  top: calc(50% - 115px);
+  left: calc(50% - 400px);
+}
+
+.center-text {
+  width: 615px;
+  height: 100px;
+  font-size: 80px;
   margin: 0 auto;
-  margin-top: 200px;
+  text-align: center;
+  color: white;
+  font-family: 'Ubuntu', sans-serif;
+}
+
+.header-info {
+  padding: 10px;
+  width: 500px;
+  height: auto;
+  margin: 20px auto;
+  text-align: center;
+  background: black;
+  opacity: 0.6;
+  border-radius: 10px;
+  font-size: 16px;
+  color: #eaeadf;
+  font-weight: 600;
+}
+.header-info p{
+  margin: 0 0 15px 0;
+}
+.top-social_v2 {
+  height: 100%;
+}
+
+.top-social-logo {
+  height: 40px;
+}
+
+.top-social-logo li {
+  width: 60px;
+  height: 40px;
+  float: left;
 }
 </style>
